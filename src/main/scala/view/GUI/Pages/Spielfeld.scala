30fe402a4433f4cc604253865ade68d6b842999f
smx360 <64632950+smx360@users.noreply.github.com>
2020-06-26 02:03:50 +0200

    Added Gameplay functions

--- src/main/scala/view/GUI/Pages/Spielfeld.scala
+++ src/main/scala/view/GUI/Pages/Spielfeld.scala
@@ -1,6 +1,6 @@
 package view.GUI.Pages
 
-import java.awt.FlowLayout
+import java.awt.{Button, Color, FlowLayout}
 
 import control.{Controller, UpdateGuiEvent}
 import javax.swing.ImageIcon
@@ -8,54 +8,59 @@ import model.Player
 import view.GUI.InfoBar
 
 import scala.collection.immutable
-import scala.swing.event.{ MouseClicked}
-import scala.swing.{BoxPanel, Dimension, GridPanel, Label, ListView, Orientation,  Swing}
+import scala.swing.event.MouseClicked
+import scala.swing.{BoxPanel, Dimension, GridPanel, Label, ListView, Orientation, Swing}
 
 class Spielfeld(controller: Controller, infoBar: InfoBar) extends BoxPanel(Orientation.Vertical) {
+
   preferredSize = new Dimension(790, 500)
+  background = Color.BLACK
 
   var listString = List[String]()
-  for(x <- controller.gameManager.player(controller.gameManager.activePlayer).playerCards){
+
+  for (x <- controller.gameManager.player(controller.gameManager.activePlayer).playerCards) {
     listString = listString :+ x.antwort
   }
-  val roundLbl = new Label("Frage der Runde" + controller.gameManager.numberOfRounds+" : " + controller.gameManager.roundQuestion)
+
+  //val roundLbl = new Label("Frage der Runde" + controller.gameManager.numberOfRounds + " : " + controller.gameManager.roundQuestion)
+
+  infoBar.text = controller.gameManager.roundQuestion
+
   var playedCard = 0
 
   contents += new BoxPanel(Orientation.Horizontal) {
+    background = Color.GREEN
     contents += new Label("Player: " + controller.gameManager.activePlayer + 1)
     contents += Swing.HGlue
     contents += new Label("Round " + controller.gameManager.numberOfRounds)
-    contents += roundLbl
+    //contents += roundLbl
   }
+
   contents += Swing.HGlue
-  contents += new ListView[String](listString){
+
+  contents += new ListView[String](listString) {
     listenTo(mouse.clicks)
-    reactions +={
-      case b : MouseClicked if b.clicks == 2 =>
-            playedCard = selection.anchorIndex
-            controller.eval(playedCard.toString)
+
+    reactions += {
+      case b: MouseClicked if b.clicks == 2 =>
+        playedCard = selection.anchorIndex
+        controller.eval(playedCard.toString)
     }
   }
+
   contents += Swing.HGlue
-  contents += new GridPanel(controller.gameManager.player.length,1){
-    for(i <- controller.gameManager.roundAnswerCards ){
+
+  contents += new GridPanel(controller.gameManager.player.length, 1) {
+
+    background = Color.BLUE
+
+    for (i <- controller.gameManager.roundAnswerCards) {
       contents += new Label("Spieler " + i._1.name + " hat " + i._2 + " gespielt")
-
     }
+
   }
 
-
-
   contents += Swing.HGlue
-
-
-
-
-
-
-
-
-
   /*val panelCenter = new BoxPanel(Orientation.Vertical) {
     background = Color.PINK
     contents += Swing.HStrut(150)
@@ -85,8 +90,6 @@ class Spielfeld(controller: Controller, infoBar: InfoBar) extends BoxPanel(Orien
       contents +=new ListView[String](ListOfListViews(2)){
       Orientation.Vertical
     }
-
-
     contents += new Button("Test")
   }
   val panelEast = new BoxPanel(Orientation.Vertical) {
@@ -100,12 +103,9 @@ class Spielfeld(controller: Controller, infoBar: InfoBar) extends BoxPanel(Orien
     contents += new Button("Test")
   }*/
 
-
-
-
   //add(panelNorth, BorderPanel.Position.North)
   //add(panelSouth, BorderPanel.Position.South)
- // add(panelWest, BorderPanel.Position.West)
+  // add(panelWest, BorderPanel.Position.West)
   //add(panelEast, BorderPanel.Position.East)
- // add(panelCenter, BorderPanel.Position.Center)
+  // add(panelCenter, BorderPanel.Position.Center)
 }
