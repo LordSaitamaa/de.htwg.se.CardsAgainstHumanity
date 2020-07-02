2ffb130ad8654c2031a45c6e7954bf14cceee095
smx360 <simon.kaemmer@yahoo.de>
2020-07-02 16:28:31 +0200

    Added ControllerInterface

--- src/main/scala/control/ControllerInterface.scala
+++ src/main/scala/control/ControllerInterface.scala
@@ -2,4 +2,10 @@ package control
 
 trait ControllerInterface {
 
+  def changePage(page: Int)
+  def eval(input: String)
+  def stateAsString
+  def getCurrentStateAsString
+  def undo
+  def redo
 }
