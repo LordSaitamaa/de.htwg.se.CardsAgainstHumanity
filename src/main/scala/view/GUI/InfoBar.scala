2a49d8756ee0d57a0984c6d53d4bc5ddf3abf85d
smx360 <simon.kaemmer@yahoo.de>
2020-07-10 01:35:16 +0200

    Changed GUI to a better look

--- src/main/scala/view/GUI/InfoBar.scala
+++ src/main/scala/view/GUI/InfoBar.scala
@@ -1,10 +1,17 @@
 package view.GUI
 
-import scala.swing.{Alignment, Dimension, TextField}
+import java.awt.Color
+
+import scala.swing.Swing.EmptyBorder
+import scala.swing.{Alignment, Dimension, Font, TextField}
 
 class InfoBar extends TextField{
 
-  preferredSize = new Dimension(790, 100)
+  preferredSize = new Dimension(790, 75)
   editable = false
+  background = Color.BLACK
+  border = EmptyBorder
+  foreground = Color.WHITE
+  font = Font("System", Font.Bold, 18)
   horizontalAlignment = Alignment.Center
 }
--- src/main/scala/view/GUI/Pages/StartPage.scala
+++ src/main/scala/view/GUI/Pages/StartPage.scala
@@ -1,31 +1,62 @@
 package view.GUI.Pages
 
-import java.awt.Color
+import java.awt.{Color, Toolkit}
+
 import scala.swing._
 import scala.swing.{Action, BorderPanel, Button, ComboBox, Dimension, FlowPanel, Label, Menu, MenuItem, TextField}
 import control.ControllerInterface
+import javax.swing.ImageIcon
+import scala.swing.Swing.EmptyBorder
 import view.GUI.InfoBar
 
 import scala.swing.event._
 
 class StartPage(controller: ControllerInterface, infobar: InfoBar) extends BorderPanel {
 
-  val startBtn = new Button("Spiel starten")
-  val titleLbl = new Label("CARDS AGAINST THE HUMANITY") {
+  val startBtn = new Button("Spiel starten"){
+    background = Color.BLACK
+    foreground = Color.WHITE
+    preferredSize = new Dimension(200, 50)
+    font = Font("System", Font.Bold, 18)
+    border = EmptyBorder
+  }
+  val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+  /*val titleLbl = new Label("CARDS AGAINST THE HUMANITY") {
     font = new Font("Arial-Black", 3, 30)
   }
+
+   */
   val anzahlSpielerCb = new ComboBox(List(2, 3, 4))
   listenTo(controller)
 
   def mainPanel = new FlowPanel {
 
-    background = Color.LIGHT_GRAY
 
-    contents += startBtn
-    contents += anzahlSpielerCb
+    background = Color.BLACK
+    private val pic1 = new ImageIcon("src/main/images/CaH.png").getImage
+    private val resize = pic1.getScaledInstance(900, 400, java.awt.Image.SCALE_SMOOTH)
+    private val pic2 = new ImageIcon("src/main/images/aGame.png").getImage
+    private val resize2 = pic2.getScaledInstance(900, 250, java.awt.Image.SCALE_SMOOTH)
+    contents += new Label{
+      icon = new ImageIcon(pic1)
+    }
+    contents += new FlowPanel{
+
+      background = Color.BLACK
+      foreground = Color.WHITE
+      font = Font("System", Font.Bold, 18)
+      preferredSize = new Dimension(screenSize.width, 50)
+      maximumSize = new Dimension(screenSize.width, 50)
+      contents += startBtn
+      contents += anzahlSpielerCb
+    }
+    contents += new Label{
+      icon = new ImageIcon(pic2)
+    }
+
   }
 
-  add(titleLbl, BorderPanel.Position.North)
+  //add(titleLbl, BorderPanel.Position.North)
   add(mainPanel, BorderPanel.Position.Center)
 
   infobar.text = "Spielerzahl auswählen!"
@@ -41,8 +72,9 @@ class StartPage(controller: ControllerInterface, infobar: InfoBar) extends Borde
           infobar.foreground = Color.WHITE;
           infobar.text = "Spielerzahl darf nicht 0 sein!"
         } else {
-          infobar.background = Color.WHITE;
-          infobar.foreground = Color.BLACK;
+          infobar.background = Color.BLACK;
+          infobar.foreground = Color.WHITE;
+          infobar.font = Font("System", Font.Bold, 18)
           controller.eval(anzahlSpielerCb.item.toString)
           infobar.text = controller.getCurrentStateAsString()
         }
